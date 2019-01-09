fcede521848f032ea3aec4a2b8aefc753ac9329f
corinnegottleb <corinneottlieb@gmail.com>
2019-01-09 15:22:58 +0200

    exercise 1 complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import '../styles/home.css'
 
 class Home extends Component {
 
     render() {
         return (
+            <Router>
             <div>
                 <div id="u-input">
                     <input type="text" placeholder="Ask and you shall receive" />
@@ -15,13 +17,14 @@ class Home extends Component {
                 <h1 id="home-title">Your Adventure</h1>
 
                 <div id="home-container">
-                    <div id="world"><span className="main-directory-text">World</span></div>
-                    <div id="wizards"><span className="main-directory-text">Wizards</span></div>
-                    <div id="bestiary"><span className="main-directory-text">Bestiary</span></div>
-                    <div id="potions"><span className="main-directory-text">Potions</span></div>
-                    <div id="deities"><span className="main-directory-text">Deities</span></div>
+                   <Link to="/directory/world"> <div id="world" class="fentity-container"><span className="main-directory-text">World</span></div></Link>
+                    <Link to="/directory/wizards"><div id="wizards" class="fentity-container"><span className="main-directory-text">Wizards</span></div></Link>
+                    <Link to="/directory/bestiary"><div id="bestiary" class="fentity-container"><span className="main-directory-text">Bestiary</span></div></Link>
+                    <Link to="/directory/potions"><div id="potions" class="fentity-container"><span className="main-directory-text">Potions</span></div></Link>
+                    <Link to="/directory/potions"><div id="deities" class="fentity-container"><span className="main-directory-text">Deities</span></div></Link>
                 </div>
             </div>
+            </Router>
         );
     }
 }
